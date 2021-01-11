4ee89a3a5afab17ffb2520da25aa9dd9149617a1
RussellLin <superbr7789@gmail.com>
2021-01-11 23:31:10 +0800

    feat: Vue init

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'ad-platform',
+    component: () => import('../views/AdPlatform.vue')
+  },
+  {
+    path: '*',
+    name: 'not-found',
+    component: () => import('../views/NotFound.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/AdPlatform.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="ad-sidebar">Hello</div>
+</template>
+
+<style scoped>
+</style>
